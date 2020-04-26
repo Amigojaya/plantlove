46ac2eded1e35d1b0812898e6532d7be2e101e15
jayamouli <57309149+jayamouli@users.noreply.github.com>
2020-04-26 15:59:44 +0530

    created home controlller with index and dashboard actions/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/dashboard'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
