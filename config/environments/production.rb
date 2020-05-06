3bc952f8789a13b8936d0502ed3a1d4e0242fd9b
jayamouli <57309149+jayamouli@users.noreply.github.com>
2020-05-07 00:06:45 +0530

    latest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "ENV["MAIL"]",
+    :user_name            => ENV["MAIL"],
     :password             => ENV["APP_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
